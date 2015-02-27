07de0f61b079f3eb65804cb46c0821cd007ae029
Steven Hilton <mshiltonj@gmail.com>
2015-02-27 08:11:55 -0500

    cursor controls and booster graphics showing

--- src/js/level1.js
+++ src/js/level1.js
@@ -7,11 +7,59 @@ AndRes.Level1.prototype = {
 
   create: function(){
     this.game.add.text(10,10, "THIS IS LEVEL1", { font: "32px Arial", fill: "#fff"});
+
+    this.boostBottom = this.game.add.sprite(10, 29, 'boost-bottom');
+    this.boostLeft = this.game.add.sprite(-7, 11, 'boost-left');
+    this.boostRight = this.game.add.sprite(31, 11, 'boost-right');
+
+    this.boostBottom.visible = false;
+    this.boostLeft.visible = false;
+    this.boostRight.visible = false;
+
     this.ship = this.game.add.sprite(10,50, 'ship');
+    //this.ship.scale.x = 3;
+    //this.ship.scale.y = 3;
+
+    this.ship.addChild(this.boostBottom);
+    this.ship.addChild(this.boostLeft);
+    this.ship.addChild(this.boostRight);
 
     this.game.physics.arcade.enable(this.ship);
 
-    this.ship.body.gravity.y = 500;
+    this.cursors = this.game.input.keyboard.createCursorKeys();
+
+    this.ship.body.gravity.y = 100;
+  },
+
+  update: function(){
+
+    this.ship.body.acceleration.x = 0;
+    this.ship.body.acceleration.y = 0;
+
+
+    if(this.cursors.up.isDown){
+      this.boostBottom.visible = true;
+      this.ship.body.acceleration.y = -200;
+
+    } else {
+      this.boostBottom.visible = false;
+    }
+
+    if(this.cursors.right.isDown){
+      this.boostLeft.visible = true;
+      this.ship.body.acceleration.x += 20;
+    } else {
+      this.boostLeft.visible = false;
+    }
+
+
+    if(this.cursors.left.isDown){
+      this.boostRight.visible = true;
+      this.ship.body.acceleration.x += -20;
+    } else {
+      this.boostRight.visible = false;
+    }
+
   }
 
 };
--- src/js/preload.js
+++ src/js/preload.js
@@ -4,6 +4,9 @@ AndRes.Preload.prototype = {
   preload: function(){
     this.game.load.spritesheet('start-button', './assets/images/start-button.png', 100, 30);
     this.game.load.image('ship', './assets/images/ship.png');
+    this.game.load.image('boost-bottom', './assets/images/boost-bottom.png');
+    this.game.load.image('boost-left', './assets/images/boost-left.png');
+    this.game.load.image('boost-right', './assets/images/boost-right.png');
   },
 
   create: function(){
